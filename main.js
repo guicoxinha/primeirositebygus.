650c6b63f162040b4c4cd853eff3723d9de96fd8
guicoxinha <145709496+guicoxinha@users.noreply.github.com>
2023-09-21 16:15:31 -0300

    meu primeiro site

---
+++ main.js
@@ -0,0 +1,18 @@
+function voltarTema(){
+    document.getElementById("fundo").style.background="black";
+    document.getElementById("fundo").style.color="white";
+}
+
+
+
+function mudarTema(){
+    document.getElementById("fundo").style.background="purple";
+    document.getElementById("fundo").style.color="black";
+}
+
+function mouseforadaimagem(){
+    document.getElementById ("imagem").src="images.jpg";
+}
+function mousedentrodaimagem(){
+    document.getElementById ("imagem").src="download.jpg";
+}
